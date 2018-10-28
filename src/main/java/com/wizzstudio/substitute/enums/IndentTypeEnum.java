27ad61037d257bbf8fa68c3ebfb760cd3e1a0e61
bjchen2 <1210279050@qq.com>
2018-10-28 23:55:25 +0800

    完成Indent类的设计

---
+++ src/main/java/com/wizzstudio/substitute/enums/IndentTypeEnum.java
@@ -0,0 +1,14 @@
+package com.wizzstudio.substitute.enums;
+
+/**
+ * 订单类型枚举类
+ * Created By Cx On 2018/10/28 19:17
+ */
+public enum IndentTypeEnum {
+    //帮我购
+    HELP_BUY,
+    //帮我递
+    HELP_SEND,
+    //随意帮
+    HELP_OTHER,
+}
--- src/main/java/com/wizzstudio/substitute/pojo/Indent.java
+++ src/main/java/com/wizzstudio/substitute/pojo/Indent.java
@@ -3,7 +3,9 @@ package com.wizzstudio.substitute.pojo;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.wizzstudio.substitute.enums.Gender;
 import com.wizzstudio.substitute.enums.IndentState;
+import com.wizzstudio.substitute.enums.IndentTypeEnum;
 import com.wizzstudio.substitute.util.serializer.Date2LongSerializer;
+import lombok.Data;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
@@ -13,26 +15,24 @@ import java.util.Date;
 
 //订单
 @Entity
+@Data
 public class Indent implements Serializable {
 
     //订单id
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+    private Integer indentId;
 
     //送货人id
-    @Column
     private String performerId;
 
     //收货人id
-    @Column
     @NotNull
     private String publisherId;
 
-    //todo 是否为帮我购（不确定是否可以用boolean）
-    @Column
     @NotNull
-    private Boolean isCompusShoppingHelp;
+    @Enumerated(EnumType.STRING)
+    private IndentTypeEnum indentType;
 
     //性别
     @NotNull
@@ -40,47 +40,37 @@ public class Indent implements Serializable {
     private Gender requireGender;
 
     //任务内容
-    @Column
     @NotNull
-    private String content;
+    private String indentContent;
 
-    //商户名
-    @Column
-    private String shopName;
-
-    //物品金额
-    @Column
-    private BigDecimal goodPrice;
-
-    //快递公司名
-    @Column
-    private String expressName;
-
-    //附加要求
-    @Column
-    private String appendRequest;
-
-    //取货地址
-    @Column
+    //订单悬赏金
     @NotNull
-    private String takeGoodAddress;
+    private BigDecimal indentPrice;
 
-    //送达地点
-    @Column
-    private String shippingAddress;
+    //取货地址ID
+    private Integer takeGoodAddressId;
 
-    //取货码
-    @Column
+    //送达地点ID
+    private Integer shippingAddressId;
+
+    //联系人姓名,不能用publisherId查，因为可能不同
+    private String publisherName;
+
+    //联系人电话,不能用publisherId查，因为可能不同
+    private Long publisherPhone;
+
+    //快递公司名，仅订单类型为帮我递时非空
+    private String companyName;
+
+    //取货码，仅订单类型为帮我递时非空
     private String pickupCode;
 
-    //订单额
-    @Column
-    @NotNull
-    private String price;
+    //物品金额，仅订单类型为帮我购时非空
+    private BigDecimal goodPrice;
 
-    //订单状态
+    //订单状态,待接单：WAIT_FOR_PERFORMER，执行中：PERFORMING，货物已送达：ARRIVED，已完成：COMPLETED
     @Enumerated(EnumType.STRING)
-    private IndentState state;
+    private IndentState indentState;
 
     //创建时间
     @Column(updatable = false,insertable = false)
@@ -92,148 +82,4 @@ public class Indent implements Serializable {
     @Column(updatable = false,insertable = false)
     @JsonSerialize(using = Date2LongSerializer.class)
     private Date updateTime;
-
-
-    public Indent() {
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getPerformerId() {
-        return performerId;
-    }
-
-    public void setPerformerId(String performerId) {
-        this.performerId = performerId;
-    }
-
-
-
-    public String getPickupCode() {
-        return pickupCode;
-    }
-
-    public void setPickupCode(String pickupCode) {
-        this.pickupCode = pickupCode;
-    }
-
-    public String getPrice() {
-        return price;
-    }
-
-    public void setPrice(String price) {
-        this.price = price;
-    }
-
-    public String getPublisherId() {
-        return publisherId;
-    }
-
-    public void setPublisherId(String publisherId) {
-        this.publisherId = publisherId;
-    }
-
-    public String getTakeGoodAddress() {
-        return takeGoodAddress;
-    }
-
-    public void setTakeGoodAddress(String takeGoodAddress) {
-        this.takeGoodAddress = takeGoodAddress;
-    }
-
-    public IndentState getState() {
-        return state;
-    }
-
-    public void setState(IndentState state) {
-        this.state = state;
-    }
-
-    public Boolean getCompusShoppingHelp() {
-        return isCompusShoppingHelp;
-    }
-
-    public void setCompusShoppingHelp(Boolean compusShoppingHelp) {
-        isCompusShoppingHelp = compusShoppingHelp;
-    }
-
-    public String getShopName() {
-        return shopName;
-    }
-
-    public void setShopName(String shopName) {
-        this.shopName = shopName;
-    }
-
-    public BigDecimal getGoodPrice() {
-        return goodPrice;
-    }
-
-    public void setGoodPrice(BigDecimal goodPrice) {
-        this.goodPrice = goodPrice;
-    }
-
-    public String getExpressName() {
-        return expressName;
-    }
-
-    public void setExpressName(String expressName) {
-        this.expressName = expressName;
-    }
-
-    public String getAppendRequest() {
-        return appendRequest;
-    }
-
-    public void setAppendRequest(String appendRequest) {
-        this.appendRequest = appendRequest;
-    }
-
-    public Date getCreateTime() {
-        return createTime;
-    }
-
-    @PrePersist
-    public void setCreateTime() {
-        this.createTime = new Date();
-    }
-
-    public Date getUpdateTime() {
-        return updateTime;
-    }
-
-    @PreUpdate
-    public void setUpdateTime() {
-        this.updateTime = updateTime;
-    }
-
-    public Gender getRequireGender() {
-        return requireGender;
-    }
-
-    public void setRequireGender(Gender requireGender) {
-        this.requireGender = requireGender;
-    }
-
-    public String getShippingAddress() {
-        return shippingAddress;
-    }
-
-    public void setShippingAddress(String shippingAddress) {
-        this.shippingAddress = shippingAddress;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
 }
