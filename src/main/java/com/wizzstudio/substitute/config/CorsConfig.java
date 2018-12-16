17d1aa06d374b5f4eb74fec298432d73d5b61411
Kikyou1997 <cyborgz1999@gmail.com>
2018-12-17 00:30:06 +0800

    fix a bug in AdminServiceImpl

---
+++ src/main/java/com/wizzstudio/substitute/config/CorsConfig.java
@@ -0,0 +1,25 @@
+package com.wizzstudio.substitute.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by Kikyou on 18-12-16
+ */
+
+@Configuration
+public class CorsConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowCredentials(true)
+                .allowedMethods("GET", "POST", "DELETE", "PUT")
+                .maxAge(3600);
+    }
+
+}
+
+
